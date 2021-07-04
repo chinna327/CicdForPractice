fccecb75b17177aaf7f61675d753489a62d85b34
mural <mural@DESKTOP-31RDGDF>
2021-07-04 09:52:09 -0400

    5th push

--- mycicd/src/test/java/com/mycicd/suites/SampleSuiteITest.java
+++ mycicd/src/test/java/com/mycicd/suites/SampleSuiteITest.java
@@ -7,6 +7,6 @@ public class SampleSuiteITest  {
 	@Test
 	  public void sampleTest() {
 
-	System.out.println("This time for CI implmentatino 4th time");
+	System.out.println("This time for CI implmentatino 5th time");
 	}
 }
